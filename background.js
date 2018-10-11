b2bd2909fd5619bee3282386040f2e0a97631b0a
Karuna Sehgal <karuna@Karuna-Sehgals-Computer.local>
2018-10-11 12:46:35 -0400

    successfully able to open a new tab with search results

--- background.js
+++ background.js
@@ -4,3 +4,11 @@
 //
 //   }
 // )
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    if(request.candidateSearchInit === "initSearch") {
+      // alert("submit clicked")
+      chrome.tabs.create({url: 'https://www.queryly.com/timestribune_search.htm?q=matt%20cartwright'});
+    }
+
+})
--- popup.js
+++ popup.js
@@ -7,12 +7,12 @@ $(function(){
     candidateName = $(this).val()
   })
 
- $('#getCandidateName').click(function(){
-   chrome.storage.sync.set({'candidateName': candidateName}, function(){
-     alert('stored'+ candidateName)
-   })
+  $('#getCandidateName').click(function(){
+    chrome.storage.sync.set({'candidateName': candidateName});
+     // alert('stored '+ candidateName)
+    chrome.runtime.sendMessage({candidateSearchInit: "initSearch"})
 
+  })
 
- })
 
 })
